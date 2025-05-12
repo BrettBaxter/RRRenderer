64d8d7fcad581cf1db1e7bb7e58fa862dcdd4eb8
Brett Baxter <156020984+BrettBaxter@users.noreply.github.com>
2025-05-12 13:27:54 -0600

    Tutorial 05 Complete.

--- hw3d/WinMain.cpp
+++ hw3d/WinMain.cpp
@@ -1,4 +1,6 @@
 #include <Windows.h>
+#include <sstream>
+#include <string>
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
@@ -19,6 +21,22 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 		{
 			SetWindowText(hWnd, "Un-Respects");
 		}
+		break;
+	case WM_CHAR:
+		{
+			static std::string title;
+			title.push_back((char)wParam);
+			SetWindowText(hWnd, title.c_str());
+		}
+		break;
+	case WM_LBUTTONDOWN:
+		{
+			const POINTS pt = MAKEPOINTS(lParam);
+			std::ostringstream oss;
+			oss << "(" << pt.x << "," << pt.y << ")";
+			SetWindowText(hWnd, oss.str().c_str());
+		}
+		break;
 	}
 	return DefWindowProc(hWnd, msg, wParam, lParam);
 }
